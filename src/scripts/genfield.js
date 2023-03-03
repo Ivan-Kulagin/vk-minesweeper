9628006099b3334a1239aee0050d329437ab14ab
Ivan Kulagin <ivan.kulagin2001@gmail.com>
2023-03-03 14:41:11 +0300

    always empty neighbors on field init

--- src/scripts/genfield.js
+++ src/scripts/genfield.js
@@ -1,7 +1,7 @@
 const shuffleArray = (array) => {
     let newArray = []
-    const fieldSize = array.length
-    for (let i = 0; i < fieldSize; i++) {
+    const arraySize = array.length
+    for (let i = 0; i < arraySize; i++) {
         let currentIndex = Math.round(Math.random() * (array.length - 1))
         newArray.push(array[currentIndex])
         array.splice(currentIndex, 1)
@@ -25,15 +25,57 @@ const chunkArray = (array, chunkSize) => {
 //     return newArray
 // }
 
-const generateField = (width, height, mines, clickPos) => {
+const getNearbySquares = (clickPos, width) => {
     const clickedSquare = clickPos.y * width + clickPos.x
+    let squares = [
+        clickedSquare,                  // clicked one
+        clickedSquare + width,          // south
+        clickedSquare - width,          // north
+    ]
+
+    if (clickedSquare % width !== 15) {
+        squares.push(
+            clickedSquare + 1,          // east
+            clickedSquare + 1 + width,  // south-east
+            clickedSquare + 1 - width   // north-east
+        )
+    }
+    if (clickedSquare % width !== 0) {
+        squares.push(
+            clickedSquare - 1,          // west
+            clickedSquare - 1 + width,  // south-west
+            clickedSquare - 1 - width   // north-west
+        )
+    }
+    squares = squares.filter(square => square > -1 && square < 256)
+    return squares
+}
+
+const generateField = (width, height, mines, clickPos) => {
+    // const clickedSquare = clickPos.y * width + clickPos.x
     const array = [...Array(width * height - mines).fill({mine: false, hidden: true}), ...Array(mines).fill({mine: true, hidden: true})]
     const shuffledArray = shuffleArray(array)
-    if (shuffledArray[clickedSquare].mine) {
-        let foundEmpty = shuffledArray.findIndex(i => !i.mine)
-        shuffledArray[foundEmpty] = {...shuffledArray[foundEmpty], mine: true}
-        shuffledArray[clickedSquare] = {...shuffledArray[clickedSquare], mine: false}
+
+    const emptySquaresPos = shuffledArray.map((square, i) => !square.mine ? i : -1).filter(idx => idx !== -1);
+    const nearbySquaresPos = getNearbySquares(clickPos, width)
+    const filteredEmptyPosArray = emptySquaresPos.filter(i => !nearbySquaresPos.includes(i))
+    const shuffledFilteredEmptyPosArray = shuffleArray(filteredEmptyPosArray)
+    console.log({emptySquaresPos, nearbySquaresPos, shuffledFilteredEmptyPosArray})
+    let iter = 0
+    for (let pos of nearbySquaresPos) {
+        if (shuffledArray[pos].mine) {
+            let foundEmpty = shuffledFilteredEmptyPosArray[iter]
+            shuffledArray[foundEmpty] = {...shuffledArray[foundEmpty], mine: true}
+            shuffledArray[pos] = {...shuffledArray[pos], mine: false}
+            iter++
+        }
     }
+
+    // if (shuffledArray[clickedSquare].mine) {
+    //     let foundEmpty = shuffledArray.findIndex(i => !i.mine)
+    //     shuffledArray[foundEmpty] = {...shuffledArray[foundEmpty], mine: true}
+    //     shuffledArray[clickedSquare] = {...shuffledArray[clickedSquare], mine: false}
+    // }
     const chunkedArray = chunkArray(shuffledArray, width)
     return calculateFieldCells(chunkedArray)
 }
